ed0c8187200ab8b3a1388fcf6656c5344b5ce666
דזימיטרובסקי אלישבע < @mbjcomp.local>
2024-03-05 14:19:15 +0200

    Add Dal Api

---
+++ FindJobs/DataBase/Dal Api/IRepository.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataBase
+{
+    public interface ICrud<T>
+    {
+            
+        void Create(T item);
+
+        void Delete(T item);
+
+        void Upadte(T item);
+        void Get(T item);
+
+
+        List<T> Read();
+    }
+}
+
--- FindJobs/FindJobs/Program.cs
+++ FindJobs/FindJobs/Program.cs
@@ -1,12 +1,14 @@
 
-
-
-
+using DataAccessLayer;
+using DataBase.Models;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 DBActions actions = new DBActions(builder.Configuration);
-var connString = actions.GetConnectionString("FactoryFoodDB");
-builder.Services.AddDbContext<Context>(options => options.UseSqlServer(connString));
+var connString = actions.GetConnectionString("AcademyDB");
+builder.Services.AddDbContext<Context>(opt => opt.UseSqlServer(connString));
+//builder.Services.AddScoped<IUniversityRepo, UniversityRepo>();
+
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
