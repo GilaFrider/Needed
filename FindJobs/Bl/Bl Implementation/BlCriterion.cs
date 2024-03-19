d76860d797834e4e9f31979922291aa1dca3840c
פרידר גילה < @mbjcomp.local>
2024-03-19 19:02:30 +0200

    last change

--- FindJobs/Bl/Bl Implementation/BlCriterion.cs
+++ FindJobs/Bl/Bl Implementation/BlCriterion.cs
@@ -1,12 +1,86 @@
-﻿using System;
+﻿using Bl.Bl_Api;
+using Bl.DTO;
+using DataBase.Dal_Api;
+using DataBase.Models;
+using DataBase;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using DataBase.Dal_Implementation;
+using System.Runtime.ConstrainedExecution;
 
 namespace Bl.Bl_Implementation
 {
-    public class BlCriterion
+    public class BlCriterion : IBlCriterion
     {
+        ICriterionRepo _CriterionRepo;
+        public BlCriterion(DalManager dalManager)
+        {
+            _CriterionRepo = dalManager.criterion;
+        }
+        public List<CriterionDTO> GetAll()
+        {
+            try
+            {
+                List<Criterion> criterions = _CriterionRepo.GetAll();
+
+
+                List<CriterionDTO> criterionDTOs = criterions.Select(criterion => new CriterionDTO
+                {
+                    Code = criterion.Code,
+                    SeveralYearsOfExperience = criterion.SeveralYearsOfExperience,
+                    Car = criterion.Car,
+                    NumberOfCvsSent = criterion.NumberOfCvsSent,
+                    Salary = criterion.Salary,
+                    Descriptions = criterion.Descriptions,
+                }).ToList();
+
+                return criterionDTOs;
+            }
+            catch (Exception ex)
+            {
+
+                throw;
+            }
+        }
+        public CriterionDTO Create(CriterionDTO criterion)
+        {
+            Criterion c = new Criterion();
+            c.Code = criterion.Code;
+            c.SeveralYearsOfExperience = criterion.SeveralYearsOfExperience;
+            c.Car = criterion.Car;
+            c.NumberOfCvsSent = criterion.NumberOfCvsSent;
+            c.Salary = criterion.Salary;
+            c.Descriptions = criterion.Descriptions;
+            _CriterionRepo.Create(c);
+            return criterion;
+        }
+
+        public CriterionDTO Delete(CriterionDTO item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public CriterionDTO Upadte(int ID, CriterionDTO item)
+        {
+            throw new NotImplementedException();
+        }
+        //public CriterionDTO Delete(string name)
+        //{
+        //    Crown c = crowns.Delete(name);
+        //    return new CrownDTO()
+        //    {
+        //        CrownCode = c.CrownCode,
+        //        Name = name,
+        //        Description = c.Description,
+        //        Price = c.Price,
+        //        ColorId = c.ColorId,
+        //        Qtty = c.Qtty,
+        //        Image = c.Image
+        //    };
     }
+
 }
+
